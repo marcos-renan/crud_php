09667e60c3dd1ea535677b277d82cb8527a027f5
Marcos <devmarcos7@gmail.com>
2025-03-02 21:01:53 -0300

    criando primeira tela

--- index.php
+++ index.php
@@ -19,8 +19,39 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Lista de usuários</h4>
-                        <a href="usuario-create.php" class="btn btn-primary float-end">Adicionar usuário</a>
+                        <h4>Lista de usuários
+                            <a href="usuario-create.php" class="btn btn-primary float-end">Adicionar usuário</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>E-mail</th>
+                                    <th>Data Nascimento</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>teste</td>
+                                    <td>teste@gmail.com</td>
+                                    <td>01/01/2001</td>
+                                    <td>
+                                        <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
+                                        <a href="" class="btn btn-success btn-sm">Editar</a>
+                                        <form action="" method="POST" class="d-inline">
+                                            <button type="submit" name="delete_usuario" value="1" class="btn btn-danger btn-sm">
+                                                Excluir
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
