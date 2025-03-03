2d761a49da86476a96299ae095b7430a81e483ec
Marcos <devmarcos7@gmail.com>
2025-03-03 08:08:55 -0300

    criada mensagem de erro ou de sucesso na criacao de usuario

--- acoes.php
+++ acoes.php
@@ -6,11 +6,21 @@ if (isset($_POST['create_usuario'])) {
     $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
     $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
     $data_nascimento = mysqli_real_escape_string($conexao, trim($_POST['data_nascimento']));
-    $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, trim($_POST['senha'])) : '';
+    $senha = isset($_POST['senha']) ? mysqli_real_escape_string($conexao, password_hash(trim($_POST['senha']), PASSWORD_DEFAULT)) : '';
 
     $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome','$email', '$data_nascimento', '$senha')";
 
     mysqli_query($conexao, $sql);
+
+    if (mysqli_affected_rows($conexao) > 0){
+        $_SESSION['mensagem'] = 'Usuário criado com sucesso!';
+        header('Location: index.php');
+        exit;
+    }else {
+        $_SESSION['mensagem'] = 'Usuário não foi criado.';
+        header('Location: index.php');
+        exit;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require('conexao.php');
 ?>
 
@@ -19,6 +20,7 @@ require('conexao.php');
     <?php include('navbar.php'); ?>
 
     <div class="container mt-4">
+        <?php include('mensagem.php'); ?>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
